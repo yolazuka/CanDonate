b16a810dbb910e919c4e32ef6dfc6505c9052dd7
yolazuka <yola.martin@gmail.com>
2017-03-22 21:16:46 +0100

    update

--- app/src/main/java/com/example/usuario/candonate/activity_result_no.java
+++ app/src/main/java/com/example/usuario/candonate/activity_result_no.java
@@ -1,8 +1,12 @@
 package com.example.usuario.candonate;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
 
 public class activity_result_no extends AppCompatActivity {
 
@@ -13,4 +17,36 @@ public class activity_result_no extends AppCompatActivity {
         setContentView(R.layout.activity_result_no);
     }
 
+
+    public void send_results_mail (View view) {
+
+
+    /* Create an intent in order to get the parameters from the Activity_Quizz.java
+         */
+
+        Intent GoToResultNo = getIntent();
+        Bundle bundle = GoToResultNo.getExtras();
+
+        /***Create the strings for the test details that will go into the email  */
+
+        String resumenDeTest = getString(R.string.email_header) + "Age: " + bundle.getInt("Age") + "\n" + getString(R.string.amount_incompatibilities) + Activity_Quizz.totalScore + "\n" + getString(R.string.Thank_you);
+
+        /***Create the email including all the parameters  */
+
+        Intent SendToEmail = new Intent(Intent.ACTION_SENDTO);
+        GoToResultNo.putExtra("Myvalues", bundle);
+        SendToEmail.setData(Uri.parse("mailto:" + bundle.getString("Email"))); // only email apps should handle this*
+        SendToEmail.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.Subject) + bundle.getString("Nombre"));
+        SendToEmail.putExtra(Intent.EXTRA_TEXT, resumenDeTest);
+
+
+
+
+        /* We write the line below to avoid the Crash just in case the app can not find a proper
+        external app*/
+
+        if (SendToEmail.resolveActivity(getPackageManager()) != null) {
+            startActivity(SendToEmail);
+        }
+    }
 }

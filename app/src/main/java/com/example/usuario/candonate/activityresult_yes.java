7fed122109edb21a82ea399ab24f158aafbb9637
yolazuka <yola.martin@gmail.com>
2017-03-22 21:17:14 +0100

    update

--- app/src/main/java/com/example/usuario/candonate/activityresult_yes.java
+++ app/src/main/java/com/example/usuario/candonate/activityresult_yes.java
@@ -8,7 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import static com.example.usuario.candonate.R.string.email_header;
+import static com.example.usuario.candonate.R.string.Subject;
+import static com.example.usuario.candonate.R.string.name;
 
 public class activityresult_yes extends AppCompatActivity {
 
@@ -16,42 +17,53 @@ public class activityresult_yes extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activityresult_yes);
+        setContentView(R.layout.activityresult_yes);
     }
 
+    public void send_results_mail (View view) {
 
-    EditText userName = (EditText) findViewById(R.id.name);
-    String user_Name = userName.getText().toString();
-
-    EditText emailAddress = (EditText) findViewById(R.id.email_address);
-    String email_Address = emailAddress.getText().toString();
-
-    EditText userAge = (EditText) findViewById(R.id.age);
-    String user_Age = userAge.getText().toString();
-
-    TextView testResume =(TextView) findViewById(R.id.testresume);
-    String resumenDeTest = testResume.getText().toString();
-
-    public void send_results_mail (View view){
-
-        /***Create the strings for the test details  */
-
-        String resumenDeTest = getString(R.string.email_header) + "\n" + getString(R.string.amount_incompatibilities) + Activity_Quizz.totalScore +"\n" + getString(R.string.Thank_you);
-
-    /* Create an intent in order to get the order in an opened email, where the mail body is the
-        test Resume and the subject is an specific one
+        /* Create an intent in order to get the parameters from the Activity_Quizz.java
          */
 
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:" + email_Address)); // only email apps should handle this*
-        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.Subject));
-        intent.putExtra(Intent.EXTRA_TEXT, resumenDeTest);
+        Intent GoToResultYes = getIntent();
+        Bundle bundle = GoToResultYes.getExtras();
+
+        /***Create the strings for the test details that will go into the email  */
+
+        String resumenDeTest = getString(R.string.email_header) + "Age: " + bundle.getInt("Age") + "\n" + getString(R.string.amount_incompatibilities) + Activity_Quizz.totalScore + "\n" + getString(R.string.Thank_you);
+
+        /***Create the intent for the email including all the parameters  */
+
+        Intent SendToEmail = new Intent(Intent.ACTION_SENDTO);
+        SendToEmail.setData(Uri.parse("mailto:" + bundle.getString("Email"))); // only email apps should handle this*
+        SendToEmail.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.Subject) + bundle.getString("Nombre"));
+        SendToEmail.putExtra(Intent.EXTRA_TEXT, resumenDeTest);
 
         /* We write the line below to avoid the Crash just in case the app can not find a proper
-        external app
-       */
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+        external app*/
+
+        if (SendToEmail.resolveActivity(getPackageManager()) != null) {
+            startActivity(SendToEmail);
         }
     }
+
+
+    public void buttonToFindCenters (View view) {
+
+            Intent goToMap = new Intent(Intent.ACTION_VIEW);
+            goToMap.setData(Uri.parse("https://www.google.es/maps/search/centro+donacion+sangre/@37.8180688,-1.4733224,5z/data=!3m1!4b1?hl=es"));
+            startActivity(goToMap);
+
+        /* We write the line below to avoid the Crash just in case the app can not find a proper
+        external app*/
+
+        if (goToMap.resolveActivity(getPackageManager()) != null) {
+            startActivity(goToMap);
+        }
+    }
+
 }
+
+
+
+
